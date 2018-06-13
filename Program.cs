106321ce0dbebc5793fc4f9468edde7a6896e9b7
jyxjjj <773933146@qq.com>
2018-06-13 23:04:01 +0800

    Finished
    此应用使用Unlicense进行授权，这意味着作者将放弃此应用的版权
    原因是此应用过于简单基础
    此应用展示了中国江苏南京如何使用PPPOE连接入校园网
    使用此应用以替代天翼客户端

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PPPOE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunCmd("Rasdial.exe", " 宽带连接 17300000000@njxy " + Console.ReadLine());
+            Console.ReadKey();
+        }
+
+        static void RunCmd(string cmdExe, string cmdStr)
+        {
+            try
+            {
+                using (Process myPro = new Process())
+                {
+                    //指定启动进程是调用的应用程序和命令行参数
+                    ProcessStartInfo psi = new ProcessStartInfo(cmdExe, cmdStr);
+                    myPro.StartInfo = psi;
+                    myPro.Start();
+                    myPro.WaitForExit();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
